5c9c3747034e70b6d12a6d44bcbe3c1a97b524ec
Azzouz <azzouz.mohamed238@gmail.com>
2023-07-01 18:36:38 +0200

    Add navBar.html && First Page

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link 
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
+    rel="stylesheet"  type='text/css'>
+    <link rel="stylesheet" href="style.css">
+    <title>Accueil</title>
+</head>
+<body>  
+    <?php
+    include_once('navBar.html')
+    ?>
+    <main id="indexMain">
+        <section id="infoFormation">
+            <h1>Le parcour Graines de Talents</h1>
+
+            <div id="interviewProgram">
+
+            </div>
+
+            <div id="detailFormation">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer mollis congue turpis, quis venenatis elit euismod rutrum. Donec ornare est vel dapibus rutrum. Phasellus ante libero, pulvinar ac purus eget, sollicitudin suscipit erat. Sed in rutrum magna. Sed hendrerit, odio eget aliquam dignissim, leo magna commodo purus, sed dapibus mauris metus sed lacus. Vestibulum convallis nec ipsum sed dapibus. Nunc vitae porttitor erat, sed laoreet urna. Nunc ut rutrum eros. Suspendisse aliquet dolor tortor, volutpat vulputate nisl euismod vitae. Etiam consectetur blandit lorem, in interdum elit blandit non. Mauris ullamcorper pharetra nulla at auctor. Donec sodales, mi eu tincidunt accumsan, tellus tellus tempus nulla, sed convallis enim neque in velit. Cras vel est turpis. Nulla scelerisque orci eu suscipit suscipit. Nullam non augue lectus.
+                <br>
+                Pellentesque lorem sem, tempus et urna cursus, imperdiet volutpat velit. Nulla elit justo, sodales eu nisi ut, fringilla sollicitudin dui. Morbi maximus dui sed lobortis congue. Morbi egestas, nisi vel molestie mollis, massa magna semper ante, sed mattis nulla mauris eu ligula. Ut neque dolor, elementum sed dictum ultricies, finibus in nunc. Ut volutpat elit ligula, in viverra urna euismod in. Praesent in ex lacinia, malesuada ligula mollis, semper justo.
+            </div>
+
+        </section>
+    </main>
+
+    <footer>
+        <section>
+            <img src="img/logo.png" alt="">
+
+            <div>
+                <ul class="listNav">
+                    <li><a href="index.html">Index</a></li>
+                    <li><a href="nosgraines.html">Nos Graines de Talents</a></li>
+                    <li><a href="gallerie.html">Gallerie Photos</a></li>
+                    <li><a href="jpo.html">Notre Journée Portes Ouvertes</a></li>
+                </ul>
+            </div>
+
+            <div>
+                <a href="test.html"><i class="fa-brands fa-facebook"></i></a>
+
+                <a href="test.html"><i class="fa-brands fa-instagram"></i></a>
+
+                <a href="test.html"><i class="fa-brands fa-linkedin"></i></a>
+            </div>
+
+        </section>
+
+    </footer>
+    
+</body>
+</html>
